b7231607a920c33affbee2e5347e6face1be37f6
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-12-18 03:36:06 +0600

    Delete React_Basics/React_Mapping_Components directory

--- React_Basics/React_Mapping_Components/src/components/Avatar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Avatar(props) {
-  return <img className="circle-img" src={props.img} alt="avatar_img" />;
-}
-
-export default Avatar;
--- React_Basics/React_Mapping_Components/src/components/Detail.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Detail(props) {
-  return <p className="info">{props.detailInfo}</p>;
-}
-
-export default Detail;
