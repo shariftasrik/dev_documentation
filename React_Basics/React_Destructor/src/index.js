154ad8067e936ef2866ef221dd8fb2aa0c24608d
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-12-20 15:39:15 +0600

    Add files via upload

---
+++ React_Basics/React_Destructor/src/index.js
@@ -0,0 +1,54 @@
+// import animals, { useAnimals } from "./data";
+
+// //Destructuring Arrays
+// // console.log(animals);
+// const [cat, dog] = animals;
+// // console.log(cat);
+
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+
+// //Destructuring Objects
+// // const { name, sound} = cat;
+// // const { name: catName, sound: catSound } = cat;
+// // const { name = "Fluffy", sound = "Purr" } = cat;
+// // const {feedingRequirements: {food, water} } = cat;
+// // console.log(food);
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+
+console.log(cars);
+const [honda, tesla] = cars;
+
+const {speedStats:{topSpeed : hondaTopSpeed, zeroToSixty : hondaZeroToSixty}} = honda;
+const {speedStats:{topSpeed : teslaTopSpeed, zeroToSixty : telsaZeroToSixty}} = tesla;
+
+const {coloursByPopularity: [hondaTopColour]} = honda;
+const {coloursByPopularity: [teslaTopColour]} = tesla;
+
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
