160ee1285817263429c55a247e8682c168dd3f47
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-12-21 00:57:34 +0600

    Add files via upload

---
+++ React_Basics/React_Changing_Complex_State/src/components/App.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+function handleChange(event){
+  const {value, name} = event.target;
+
+  setFullName((prevValue)=>{
+    if(name === "fName"){
+      return{
+        fName : value,
+        lName : prevValue.lName 
+      }
+    }else if(name === "lName"){value
+      return{
+        fName : prevValue.fName,
+        lName : value
+      }
+    }
+  })
+}
+
+
+  return (
+    <div className="container">
+      <h1>Hello {fullName.fName} {fullName.lName} !</h1>
+      <form>
+        <input 
+          name="fName" 
+          onChange={handleChange} 
+          placeholder="First Name" 
+          value={fullName.fName}
+        />
+        <input 
+          name="lName" 
+          onChange={handleChange} 
+          placeholder="Last Name" 
+          value={fullName.lName}
+        />
+        <button >Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
