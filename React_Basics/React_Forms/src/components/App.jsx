06bfd048df7f22ae5397c3a7094e2f296ce2e131
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2024-12-20 21:57:55 +0600

    Add files via upload

---
+++ React_Basics/React_Forms/src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const [name,setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleChange(event){
+    //console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+
+  function handleClick(event){
+    setHeadingText(name);
+
+    event.preventDefault();
+  }
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+          <input 
+            onChange={handleChange} 
+            type="text" 
+            placeholder="What's your name?" 
+            value={name}
+          />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
