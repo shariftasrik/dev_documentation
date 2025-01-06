f84e4804ad3de79d57e566d07fda745f89a28ed2
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2025-01-07 02:31:40 +0600

    Create datatype.js

---
+++ Stack_Learner/JavaScript/Chapter_1/datatype.js
@@ -0,0 +1,49 @@
+/// ******* Numbers ******** ////
+
+var n = 1451
+var f = 3.14159
+var nn = Number('45.45')
+console.log(nn)
+console.log(Number.parseInt(nn))
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(1/0)
+console.log('abc' * 10)
+
+
+/// ******* String ******** ////
+
+///Literal
+var str1 = 'String'
+var str2 = "Sring"
+var str3 = `String` 
+console.log(str1, str2, str3)
+
+///String constructor
+var str4 = String('abababa')
+var str5 = String(156)
+var str6 = String(3.1416)
+console.log(str4, str5, str6)
+
+
+
+
+/// *********** Boolean ********** ///
+
+var b1 = true
+var b2 = false
+
+var b3 = Boolean(true)
+var b4 = Boolean(false)
+
+console.log(b1, b2, b3, b4)
+
+
+
+/// ********* Null vs Undefined ********* ///
+
+var abc;
+console.log(abc)
+
+var xyz = null
+console.log(xyz)
