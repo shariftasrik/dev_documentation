a90a9c507242c5921939af00a0f8f67b561cbfca
Nimur Rahman Sharif Tasrik <126714137+shariftasrik@users.noreply.github.com>
2025-01-07 02:30:49 +0600

    Create variable.js

---
+++ Stack_Learner/JavaScript/Chapter_1/variable.js
@@ -0,0 +1,7 @@
+var name = "Nimur Rahman Sharif Tasrik";
+var age = 23;
+
+console.log(name + "knows Javascript");
+console.log("His age is only " + age);
+console.log(name + "is creating a Javascript code");
+console.log('But his age is only '+ age);
